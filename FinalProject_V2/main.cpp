cfcefdee9d1a7d8717e3547eb614af45c0b2a994
Tom <tom.lucero@me.com>
2019-04-17 14:01:20 -0600

    Initial Commit

---
+++ FinalProject_V2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FinalProject_V2
+//
+//  Created by Tom Lucero on 4/17/19.
+//  Copyright © 2019 Tom Lucero. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
